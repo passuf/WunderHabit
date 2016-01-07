3bdb836fa5ddf0c2982fe899ea38fc0117905693
Pavel Savchenko <pavel@modlinltd.com>
2016-01-07 14:50:18 +0100

    Feature: migrate to `philadams/habitica` package
    * prefix own `habitica` app to `wh_habitica`
    * set `Habitica` model table name to previous app name (to avoid migrating)
    * keep HabiticaApi wrapper in place
    * update imports all around

--- habitica/api.py
+++ habitica/api.py
@@ -1,55 +1,25 @@
-import requests
-import urllib
-
+import habitica as hlib
 from . import default
 
 
-class HabiticaApi:
+class HabiticaApi(hlib.api.Habitica):
     def __init__(self, user_id, api_token):
-        self.user_id = user_id
-        self.api_token = api_token
-        self.headers = {default.AUTH_HEADER_CLIENT: user_id,
-                        default.AUTH_HEADER_TOKEN: api_token}
+        headers = {default.AUTH_HEADER_CLIENT: user_id,
+                   default.AUTH_HEADER_TOKEN: api_token}
+        super(HabiticaApi, self).__init__(headers)
 
-    @staticmethod
-    def status():
+    def status(self):
         """
         Returns the Habitica server status.
         """
 
-        r = requests.get(default.GET_STATUS)
-
-        if not r or not r.status_code == 200:
-            return None
-
-        try:
-            status = r.json()
-            if status[default.JSON_STATUS] == default.JSON_UP:
-                return True
-        except Exception:
-            return None
-
-        return False
+        return self.server()[default.JSON_STATUS] == default.JSON_UP
 
     def get_user(self):
         """
         Returns the full user object.
         """
-
-        r = requests.get(default.GET_USER, headers=self.headers)
-
-        if not r or not r.status_code == 200:
-            return None
-
-        try:
-            user = r.json()
-            user_info = user[default.JSON_AUTH][default.JSON_LOCAL]
-            if default.JSON_USERNAME not in user_info or default.JSON_EMAIL not in user_info:
-                return None
-        except Exception:
-            return None
-
-        return user
+        return self.user()
 
     def post_task(self, task_id, up=True):
         """
@@ -61,21 +31,7 @@ class HabiticaApi:
         else:
             score = default.JSON_DOWN
 
-        r = requests.post(
-                default.POST_TASK.format(id=urllib.quote_plus(task_id), direction=score),
-                headers=self.headers,
-                json={},
-        )
-
-        if not r or not r.status_code == 200:
-            return None
-
-        try:
-            result = r.json()
-        except Exception:
-            return None
-
-        return result
+        return self.user.tasks(_id=task_id, _direction=score, _method='post')
 
     def test_auth(self):
         """
