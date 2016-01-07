5e0396957ebfffffeac2003d317927183661a7cd
Jonas Passerini <passuf@gmail.com>
2016-01-07 17:17:42 +0100

    Added support for Habiticas accounts authenticated with facebook. Fixes #1

--- wh_habitica/forms.py
+++ wh_habitica/forms.py
@@ -6,8 +6,7 @@ from . import default
 from .models import Habitica
 from .api import HabiticaApi
 
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('wunderhabit')
 
 
 class AuthForm(forms.ModelForm):
@@ -24,15 +23,25 @@ class AuthForm(forms.ModelForm):
 
         api = HabiticaApi(cleaned_data['user_id'], cleaned_data['api_token'])
 
-        user = None
         try:
-            user = api.get_user()
-            user_info = user[default.JSON_AUTH][default.JSON_LOCAL]
-            self.instance.name = user_info[default.JSON_USERNAME]
-            self.instance.email = user_info[default.JSON_EMAIL]
+            user_details = api.get_user_details()
         except Exception:
-            logger.exception('Exception during Habitica Auth: ' + str(user))
+            logger.exception('Could not load Habitica user details.')
+            raise forms.ValidationError(
+                    _('Could not authenticate to Habitica. Please check the User ID and the API Token.'))
+
+        # Validate authentication
+        if not user_details or cleaned_data['user_id'] != user_details[default.JSON_ID]:
             raise forms.ValidationError(
                 _('Could not authenticate to Habitica. Please check the User ID and the API Token.'))
+
+        # Get user details
+        try:
+            self.instance.name = user_details[default.JSON_NAME]
+            self.instance.email = user_details[default.JSON_EMAIL]
+        except ValueError:
+            logger.exception('Could not get user details: ' + str(user_details))
+            raise forms.ValidationError(
+                    _('Something went wrong while connecting with Habitica. Please try again or contact the admin.'))
 
         return cleaned_data
