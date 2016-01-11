46adbeb7e8fa2cd10b9e49e1852e9e6cc868d6d4
Jonas Passerini <passuf@gmail.com>
2016-01-11 21:18:48 +0100

    Feature: Added fixed footer and created templates for the Contact and Privacy pages.

--- wunderhabit/urls.py
+++ wunderhabit/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from . import views
 from wunderlist import urls as wunderlist_urls
@@ -9,6 +10,8 @@ from wh_habitica import urls as habitica_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^contact/$', TemplateView.as_view(template_name='wunderhabit/contact.html'), name='contact'),
+    url(r'^privacy/$', TemplateView.as_view(template_name='wunderhabit/privacy.html'), name='privacy'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^account/delete/$', views.delete_account, name='delete_account'),
